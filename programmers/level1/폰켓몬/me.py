97d8da676e2dc59fc490134f398730f2d110bd0a
wooy0ng <yygg9800@naver.com>
2022-07-06 22:57:59 +0900

    commit

--- programmers/level1/폰켓몬/me.py
+++ programmers/level1/폰켓몬/me.py
@@ -2,7 +2,7 @@ def solution(nums):
     nums_len = len(nums) // 2
     set_len = len(set(nums))
 
-    return set_len if nums_len >= set_len else nums_len
+    return set_len if nums_len > set_len else nums_len
 
 nums = [3,1,2,3]		
 print(solution(nums))
