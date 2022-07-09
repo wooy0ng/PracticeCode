5f327cbe88f91a396d26c952dadf6e0a4b18ee48
wooy0ng <yygg9800@naver.com>
2022-07-09 16:01:21 +0900

    commit

---
+++ programmers/level1/약수의 개수와 덧셈/me.py
@@ -0,0 +1,17 @@
+def solution(left, right):
+    def get_divisor(n):
+        return [k for k in range(1, (n//2)+1) if n % k == 0] + [n]
+    
+    answer = 0
+    for n in range(left, right+1):
+        if len(get_divisor(n)) % 2 != 0:
+            answer -= n
+        else:
+            answer += n
+    
+    return 
+
+
+left = 13
+right = 17 
+print(solution(left, right))
---
+++ programmers/level1/약수의 개수와 덧셈/solution.py
@@ -0,0 +1,9 @@
+def solution(left, right):
+    answer = 0
+    # 제곱수는 약수의 개수가 홀수이다.
+    for i in range(left,right+1):
+        if int(i**0.5)==i**0.5:
+            answer -= i
+        else:
+            answer += i
+    return answer
